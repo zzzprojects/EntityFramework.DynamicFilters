fe754521b519b1a49a55ea48cbc3c3f95cac65d0
John Cachat <jcachat1@gmail.com>
2014-10-20 23:17:14 -0400

    Multiple filters on same entity now generate filter conditions in same binding/sub-select

--- src/EntityFramework.DynamicFilters/DynamicFilterQueryVisitor.cs
+++ src/EntityFramework.DynamicFilters/DynamicFilterQueryVisitor.cs
@@ -13,18 +13,22 @@ namespace EntityFramework.DynamicFilters
                 .Where(mp => mp.Name.Contains("customannotation:" + DynamicFilterConstants.ATTRIBUTE_NAME_PREFIX))
                 .Select(m => m.Value as DynamicFilterDefinition);
 
-            DbExpression current = base.Visit(expression);
+            var baseResult = base.Visit(expression);
+            if (!filterList.Any())
+                return baseResult;
 
-            foreach(var filter in filterList)
+            var binding = DbExpressionBuilder.Bind(baseResult);
+
+            var edmType = binding.VariableType.EdmType as System.Data.Entity.Core.Metadata.Edm.EntityType;
+            if (edmType == null)
+                return baseResult;  //  ???
+
+            List<DbExpression> conditionList = new List<DbExpression>();
+
+            foreach (var filter in filterList)
             {
-                //  Bind the filter parameter to a sql parameter
-                var binding = DbExpressionBuilder.Bind(current);
-
                 //  Need to map through the EdmType properties to find the actual database/cspace name for the entity property.
                 //  It may be different from the entity property!
-                var edmType = binding.VariableType.EdmType as System.Data.Entity.Core.Metadata.Edm.EntityType;
-                if (edmType == null)
-                    continue;       //  ???
                 var edmProp = edmType.Properties.Where(p => p.MetadataProperties.Any(m => m.Name == "PreferredName" && m.Value.Equals(filter.ColumnName))).FirstOrDefault();
                 if (edmProp == null)
                     continue;       //  ???
@@ -33,11 +37,29 @@ namespace EntityFramework.DynamicFilters
                 var columnProperty = DbExpressionBuilder.Property(DbExpressionBuilder.Variable(binding.VariableType, binding.VariableName), edmProp.Name);
                 var param = columnProperty.Property.TypeUsage.Parameter(filter.ParameterName);
 
-                //  Creates an expression to match on the filter value *OR* a null filter value.  Null can be used to disable the filter completely.
-                current = DbExpressionBuilder.Filter(binding, DbExpressionBuilder.Or(DbExpressionBuilder.Equal(columnProperty, param), DbExpressionBuilder.IsNull(param)));
+                //  Create an expression to match on the filter value *OR* a null filter value.  Null can be used to disable the filter completely.
+                conditionList.Add(DbExpressionBuilder.Or(DbExpressionBuilder.Equal(columnProperty, param), DbExpressionBuilder.IsNull(param)));
             }
 
-            return current;
+            int numConditions = conditionList.Count;
+            switch (numConditions)
+            {
+                case 0:
+                    return baseResult;
+
+                case 1:
+                    return DbExpressionBuilder.Filter(binding, conditionList.First());
+
+                default:
+                    //  Have multiple conditions.  Need to append them together using 'and' conditions.
+                    var leftExpression = conditionList.First();
+
+                    for (int i = 1; i < numConditions; i++)
+                        leftExpression = leftExpression.And(conditionList[i]);
+
+                    return DbExpressionBuilder.Filter(binding, leftExpression);
+            }
         }
+
     }
 }
