030e62c17b4fb33a765a8918f64f90d40385e3d2
John Cachat <jcachat1@gmail.com>
2016-07-05 22:22:00 -0400

    v2 changelog & build number

--- src/EntityFramework.DynamicFilters/Properties/AssemblyInfo.cs
+++ src/EntityFramework.DynamicFilters/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha3")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
